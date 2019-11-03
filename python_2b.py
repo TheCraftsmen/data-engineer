542abc0b2b7292f1deaba21b25d0b5cb5f920f34
Blas Castro <castro.blas.martin@gmail.com>
2019-11-03 15:09:00 -0300

    second part agg excercise

---
+++ python_2b.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession, SQLContext
+from pyspark.sql.types import TimestampType
+from pyspark.sql import functions as F
+
+spark = SparkSession\
+    .builder\
+    .appName("DummyApp")\
+    .getOrCreate()
+
+sc = spark.sparkContext
+
+
+sqlContext = SQLContext(sc)
+df = sqlContext.read.\
+    load("dummy_app_data.json", format="json")
+df = df.withColumn("parsed", (F.col("event_time").cast(TimestampType())))
+df = df.withColumn("event_date", (F.col("parsed").cast("date")))
+
+df.groupBy(
+    'event_date',
+    'id',
+    'country',
+    'region',
+    'platform',
+    'language',
+    'device_brand')\
+    .agg(
+        F.sum(
+            F.when(F.col('event_type') == 'session', 1).otherwise(0)
+        ).alias('ev_session'),
+        F.sum(
+            F.when(F.col('event_type') == 'gpy', 1).otherwise(0)
+        ).alias('ev_gpy'),
+        F.count(F.col('event_type')))\
+    .save("agg_dummy_app_data.parquet", format="parquet")
